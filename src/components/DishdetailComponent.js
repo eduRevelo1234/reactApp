7a9cd7f70c017b84e7d5ad24da8953f30b35808e
Jogesh Muppala <eduardo.revelo@hotmail.com>
2020-11-04 09:56:15 -0800

    first commit

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb , BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDish({dish}){
     return(
@@ -16,44 +16,57 @@ function RenderDish({dish}){
 
     
     function RenderComments({comments}){
-        
-        const com = comments.map((comment)=>{
+        console.log("HOLA");
+       if(comments != null)
             return (
-                <div key={comment.id}>
+            <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
                     <ul class="list-unstyled">
-                        <li>{comment.comment}</li>
-            <li>-- {comment.author}, {new Intl.DateTimeFormat('en-US',{year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</li>
+                        
+                        {comments.map((comment) => {
+                            return(
+                                <li key={comment.id}>
+                                <p>{comment.comment}</p>
+                                <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US',{year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                </li>
+                            );
+                        })}
                     </ul>
+            </div>
+            );
+        else 
+            return (
+                <div>
                 </div>
             );
-        });
-        return(
-        <div>
-            {com}
-        </div>
-        )
     }
    
    
     const DishDetail = (props)=>{
-        const dish = props.dish;
-        if(dish != null){
-            return(
-                <div class="Container">
+        if(props.dish != null){
+            return (
+                <div className="container">
                 <div className="row">
-                    <div className = "col-12 col-md-5 m-1">
-                        <RenderDish dish={dish}/>
+                    <Breadcrumb>
+
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
                     </div>
-                     <div className = "col-12 col-md-5 m-1">
-                        <h4>
-                            Comments
-                        </h4>
-                        <RenderComments comments= {dish.comments} />
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
                 </div>
-            );
-            
+            );   
         }
         else{
             return(
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -13,22 +18,45 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS
     };
   }
 
   render() {
-    const HomePage = () => {
+
+    const AboutPage = () => {
       return(
-          <Home 
-          />
+          <About leaders = {this.state.leaders}/>
       );
     }
+    const HomePage = () => {
+      return(
+          <Home dish={this.state.dishes.filter((dish)=> dish.featured)[0]}
+            promotion={this.state.promotions.filter((promo)=> promo.featured)[0]}
+            leader={this.state.leaders.filter((leader)=> leader.featured)[0]}
+          />
+          
+
+      );
+    }
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+
     return (
       <div>
         <Header />
         <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route path='/menu/:dishId' component={DishWithId} />
+              <Route exact path="/contactus" component={Contact}></Route>
+              <Route exact path='/about' component={AboutPage}/>
               <Redirect to="/home" />
           </Switch>
         <Footer/>
